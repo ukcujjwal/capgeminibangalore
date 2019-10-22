82a4cd4a37a43ff90564ff973ecdbc335baabc57
ujjwal <ukcujjwal@gmail.com>
2019-10-22 14:39:57 +0530

    added calc function

---
+++ calc.py
@@ -0,0 +1,13 @@
+def add(x,y):
+    """Add Function"""
+    return x+y
+
+
+def subtract(x,y):
+    """Subtract function"""
+    return x-y
+
+
+def multiply(x,y):
+    """Multiply function"""
+    return x*y
